6e2eb535d2265f2acdf3f872738aeff3cace2bea
wangqingyun <wangqingyun1987@gmail.com>
2018-01-13 13:03:54 +0800

    added a comment

--- app/src/main/java/com/wangqingyun/learncampus/learnkotlin/extensions/GgDriver.kt
+++ app/src/main/java/com/wangqingyun/learncampus/learnkotlin/extensions/GgDriver.kt
@@ -9,6 +9,10 @@ package com.wangqingyun.learncampus.learnkotlin.extensions
  * In case of a name conflict between the members of the dispatch receiver and the extension
  * receiver, the extension receiver takes precedence. To refer to the member of the
  * dispatch receiver you can use the qualified this syntax.
+ *
+ * Extensions declared as members can be declared as open and overridden in subclasses. This means
+ * that the dispatch of such functions is virtual with regard to the dispatch receiver type,
+ * but static with regard to the extension receiver type.
  */
 
 class GgDriver {
