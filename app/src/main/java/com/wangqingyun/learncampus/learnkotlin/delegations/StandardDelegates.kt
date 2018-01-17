d11ae5aeec704fcfc67c25fc4c4608767e8f1097
qingyun.wang <wangqingyun123@yahoo.com>
2018-01-17 10:21:58 +0800

    added a demo for observable delegates

--- app/src/main/java/com/wangqingyun/learncampus/learnkotlin/delegations/StandardDelegates.kt
+++ app/src/main/java/com/wangqingyun/learncampus/learnkotlin/delegations/StandardDelegates.kt
@@ -1,6 +1,7 @@
 package com.wangqingyun.learncampus.learnkotlin.delegations
 
 import android.util.Log
+import kotlin.properties.Delegates
 
 /**
  * Created by qingyun.wang on 17/01/2018.
@@ -18,6 +19,20 @@ class LazySample {
     }
 }
 
+class ObservableSample {
+    var text: String by Delegates.observable("东胡") {
+        property, oldValue, newValue ->
+        Log.d("WQY", "$property from $oldValue to $newValue")
+    }
+
+    fun work() {
+        text = "匈奴"
+        text = "突厥"
+    }
+}
+
 fun tryStandardDelegates() {
     LazySample().work()
+
+    ObservableSample().work()
 }
