9111baef63ea488f5fe7037fcaf726188e7a4273
wangqingyun <wangqingyun1987@gmail.com>
2018-01-19 15:22:58 +0800

    demonstrate usage of Synchronized annotation

---
+++ app/src/main/java/com/wangqingyun/learncampus/learnkotlin/concurrency/SynchronizedSample.kt
@@ -0,0 +1,12 @@
+package com.wangqingyun.learncampus.learnkotlin.concurrency
+
+/**
+ * Created by wangqingyun on 19/01/2018.
+ */
+
+class Bank() {
+    @Synchronized
+    fun addMoney() {
+        // add money
+    }
+}
