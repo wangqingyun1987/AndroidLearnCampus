0f6a623e1cb99957724dc3d825e7535996944c1c
wangqingyun <wangqingyun1987@gmail.com>
2018-01-25 10:01:35 +0800

    demonstrate @JvmField and @JvmStatic

---
+++ app/src/main/java/com/wangqingyun/learncampus/learnkotlin/javainterop/Jol.java
@@ -0,0 +1,26 @@
+package com.wangqingyun.learncampus.learnkotlin.javainterop;
+
+/**
+ * Created by wangqingyun on 25/01/2018.
+ */
+
+public class Jol {
+    public static void tryIt() {
+        /* invalid call, attri is private in Kobj
+        String attri = Kobj.attri;
+         */
+        int count = Kobj.count;
+
+        String name = Kol.Companion.getName();
+        String gender = Kol.gender;
+
+        /* invalid call, not generated without @JvmStatic
+            Kol.toBj();
+        */
+        Kol.toSg();
+
+        Kol kol = new Kol();
+        int weight = kol.getWeight();
+        int height = kol.height;
+    }
+}
---
+++ app/src/main/java/com/wangqingyun/learncampus/learnkotlin/javainterop/Kol.kt
@@ -0,0 +1,31 @@
+package com.wangqingyun.learncampus.learnkotlin.javainterop
+
+/**
+ * Created by wangqingyun on 25/01/2018.
+ */
+
+object Kobj {
+    val attri = "NONE"
+
+    @JvmField
+    val count = 10
+}
+
+class Kol {
+    companion object {
+        val name = "Ali"
+
+        @JvmField
+        val gender = "Male"
+
+        fun toBj() {}
+
+        @JvmStatic
+        fun toSg() {}
+    }
+
+    val weight = 70
+
+    @JvmField
+    val height = 180
+}
