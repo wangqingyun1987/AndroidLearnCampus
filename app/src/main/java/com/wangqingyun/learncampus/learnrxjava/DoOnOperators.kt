6073328c9b22fb978f1a07c8410455574e0fd272
wangqingyun <wangqingyun1987@gmail.com>
2018-01-25 11:34:05 +0800

    demonstrate doOnError() operator

--- app/src/main/java/com/wangqingyun/learncampus/learnrxjava/DoOnOperators.kt
+++ app/src/main/java/com/wangqingyun/learncampus/learnrxjava/DoOnOperators.kt
@@ -28,7 +28,22 @@ fun tryDoOnComplete() {
             })
 }
 
+fun tryDoOnError() {
+    Observable.range(1, 5)
+            .doOnError { Log.d("WQY", "just error") }
+            .map { it - 3 }
+            .doOnError { Log.d("WQY", "subtraction error") }
+            .map { 10 / it }
+            .doOnError { Log.d("WQY", "division error") }
+            .subscribe({
+                Log.d("WQY", "received : $it")
+            }, {
+                Log.d("WQY", "error : ${it.message}")
+            })
+}
+
 fun tryDoOnOperators() {
     tryDoOnNext()
     tryDoOnComplete()
+    tryDoOnError()
 }
