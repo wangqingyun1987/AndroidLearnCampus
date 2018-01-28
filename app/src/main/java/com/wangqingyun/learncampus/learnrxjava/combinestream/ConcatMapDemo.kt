33d9ef75e874e7d497f399df56108623df05ea49
wangqingyun <wangqingyun1987@gmail.com>
2018-01-28 11:19:12 +0800

    demonstrate concatMapEager

--- app/src/main/java/com/wangqingyun/learncampus/learnrxjava/combinestream/ConcatMapDemo.kt
+++ app/src/main/java/com/wangqingyun/learncampus/learnrxjava/combinestream/ConcatMapDemo.kt
@@ -2,6 +2,8 @@ package com.wangqingyun.learncampus.learnrxjava.combinestream
 
 import android.util.Log
 import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
 /**
@@ -19,6 +21,22 @@ fun tryConcatMap() {
             }
 }
 
+fun tryConcatMapEager() {
+    Observable.just(100, 10, 30, 120)
+            .concatMapEager {
+                del ->
+                Observable.create<Int> {
+                    Log.d("WQY", "concatMapEager subscribed : $del")
+                    Thread.sleep(del.toLong())
+                    it.onNext(del)
+                    it.onComplete()
+                }.subscribeOn(Schedulers.from(Executors.newSingleThreadExecutor()))
+            }
+            .subscribe { Log.d("WQY", "concatMapEager received : $it") }
+}
+
 fun tryConcatMapDemo() {
     tryConcatMap()
+
+    tryConcatMapEager()
 }
