f34fc2a731bfcdb89cb0eaa8c27766520bc324b9
wangqingyun <wangqingyun1987@gmail.com>
2018-01-29 17:08:43 +0800

    demonstrate replay() with buffer size

--- app/src/main/java/com/wangqingyun/learncampus/learnrxjava/multicast/ReplayAndCacheDemo.kt
+++ app/src/main/java/com/wangqingyun/learncampus/learnrxjava/multicast/ReplayAndCacheDemo.kt
@@ -28,6 +28,25 @@ fun tryReplay() {
             }
 }
 
+fun tryReplayWithBufferSize() {
+    val connectable: ConnectableObservable<Long> = Observable.interval(100, TimeUnit.MILLISECONDS)
+            .take(10)
+            .replay(2)
+
+    val source = connectable.autoConnect()
+
+    source.subscribe {
+        Log.d("WQY", "A: $it")
+    }
+
+    Completable.timer(600, TimeUnit.MILLISECONDS)
+            .andThen(source)
+            .subscribe {
+                Log.d("WQY", "B: $it")
+            }
+}
+
 fun demoReplayAndCache() {
     tryReplay()
+    tryReplayWithBufferSize()
 }
