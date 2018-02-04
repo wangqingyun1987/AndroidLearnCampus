838eeac212735d71534cdaf085352de076906a50
wangqingyun <wangqingyun1987@gmail.com>
2018-02-04 11:03:34 +0800

    demonstrate parallelization using flatMap()

---
+++ app/src/main/java/com/wangqingyun/learncampus/learnrxjava/cocurrencies/ParallelUtils.kt
@@ -0,0 +1,9 @@
+package com.wangqingyun.learncampus.learnrxjava.cocurrencies
+
+/**
+ * Created by wangqingyun on 04/02/2018.
+ */
+
+fun intensiveComputation() {
+    Thread.sleep(2000)
+}
---
+++ app/src/main/java/com/wangqingyun/learncampus/learnrxjava/cocurrencies/parallelization/FlatMapParallelDemo.kt
@@ -0,0 +1,29 @@
+package com.wangqingyun.learncampus.learnrxjava.cocurrencies.parallelization
+
+import android.util.Log
+import com.wangqingyun.learncampus.learnrxjava.cocurrencies.intensiveComputation
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+
+/**
+ * Created by wangqingyun on 04/02/2018.
+ */
+
+private fun tryFlatMapParallel() {
+    Observable.range(1, 10)
+            .flatMap {
+                Observable.just(it)
+                        .subscribeOn(Schedulers.computation()) // utilizing cores available
+                        .map {
+                            intensiveComputation()
+                            it
+                        }
+            }
+            .subscribe {
+                Log.d("WQY", "received $it")
+            }
+}
+
+fun demoParallelWithFlatMap() {
+    tryFlatMapParallel()
+}
