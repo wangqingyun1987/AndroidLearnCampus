5595d77379bd556bc8d1cfc7cd97f4c5d61c3e0d
wangqingyun <wangqingyun1987@gmail.com>
2018-02-05 12:52:34 +0800

    demonstrate window() with boundary Observable

--- app/src/main/java/com/wangqingyun/learncampus/learnrxjava/flowcontrol/WindowDemo.kt
+++ app/src/main/java/com/wangqingyun/learncampus/learnrxjava/flowcontrol/WindowDemo.kt
@@ -1,8 +1,15 @@
 package com.wangqingyun.learncampus.learnrxjava.flowcontrol
 
+import android.os.Looper
 import android.util.Log
 import io.reactivex.Observable
+import io.reactivex.ObservableEmitter
+import io.reactivex.ObservableOnSubscribe
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import java.util.*
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicBoolean
 
 /**
  * Created by wangqingyun on 05/02/2018.
@@ -36,8 +43,60 @@ private fun tryWindowTimeBased() {
             }
 }
 
+private fun tryWindowBoundaryObservable() {
+    val observable = Observable.create(object: ObservableOnSubscribe<Int> {
+        private var count = 0
+        private var stopped = AtomicBoolean(false)
+        private lateinit var emitter: ObservableEmitter<Int>
+        private var disposable: Disposable? = null
+
+        private fun run() {
+            if (!stopped.get()) {
+                disposable = AndroidSchedulers.from(Looper.getMainLooper()).scheduleDirect(
+                        {
+                            emitter.onNext(++count)
+                            run()
+                        },
+                        200 + Math.abs(Random().nextLong() % 200),
+                        TimeUnit.MILLISECONDS
+                )
+            }
+        }
+
+        override fun subscribe(emitter: ObservableEmitter<Int>) {
+            this.emitter = emitter
+
+            emitter.setCancellable {
+                stopped.set(true)
+                disposable?.run {
+                    if (!isDisposed) {
+                        dispose()
+                    }
+                }
+            }
+
+            run()
+        }
+    })
+
+    Observable.interval(86, TimeUnit.MILLISECONDS)
+            .take(30)
+            .window(observable)
+            .flatMapSingle<String> {
+                it.reduce("") {
+                    accum: String, next: Long ->
+                    if (accum.isEmpty()) "$next" else "$accum + $next"
+                }
+            }
+            .subscribe {
+                Log.d("WQY", "received $it")
+            }
+}
+
 fun demoWindow() {
     tryWindowFixedSize()
 
     tryWindowTimeBased()
+
+    tryWindowBoundaryObservable()
 }
