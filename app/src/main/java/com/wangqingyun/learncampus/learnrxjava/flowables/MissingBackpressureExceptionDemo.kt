e178d468b361bbfb6421eb4486cb0c3e71d9e55c
wangqingyun <wangqingyun1987@gmail.com>
2018-02-08 11:13:59 +0800

    demonstrate MissingBackpressureException will happen when using Flowable.interval() with backpressure

---
+++ app/src/main/java/com/wangqingyun/learncampus/learnrxjava/flowables/MissingBackpressureExceptionDemo.kt
@@ -0,0 +1,25 @@
+package com.wangqingyun.learncampus.learnrxjava.flowables
+
+import android.util.Log
+import io.reactivex.Flowable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created by wangqingyun on 08/02/2018.
+ */
+
+private fun tryFlowableInterval() {
+    Flowable.interval(100, TimeUnit.MILLISECONDS)
+            .observeOn(Schedulers.newThread())
+            .subscribe ({
+                Thread.sleep(200)
+            }, {
+                Log.d("WQY", "error")
+                it.printStackTrace()
+            })
+}
+
+fun demoMissingBackpressureException() {
+    tryFlowableInterval()
+}
