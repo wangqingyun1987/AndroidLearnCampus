a531cc4a20577a57ecb4cba46c4d592e5edf2254
wangqingyun <wangqingyun1987@gmail.com>
2018-02-21 11:09:42 +0800

    demonstrate TestScheduler

---
+++ app/src/test/java/com/wangqingyun/learncampus/learnrxjava/test/nonblocking/TryTestScheduler.kt
@@ -0,0 +1,36 @@
+package com.wangqingyun.learncampus.learnrxjava.test.nonblocking
+
+import io.reactivex.Observable
+import io.reactivex.observers.TestObserver
+import io.reactivex.schedulers.TestScheduler
+import org.junit.Test
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created by wangqingyun on 21/02/2018.
+ */
+
+class TryTestScheduler {
+    @Test
+    fun verify90Emissions() {
+        val scheduler = TestScheduler()
+        val source    = Observable.interval(1, TimeUnit.MINUTES, scheduler).take(90)
+        val observer  = TestObserver<Long>()
+
+        source.subscribe(observer)
+
+        scheduler.advanceTimeBy(50, TimeUnit.SECONDS)
+
+        observer.assertNoValues()
+
+        scheduler.advanceTimeBy(50, TimeUnit.SECONDS)
+
+        observer.assertValueCount(1)
+
+        scheduler.advanceTimeBy(100, TimeUnit.MINUTES)
+
+        observer.assertComplete()
+                .assertNoErrors()
+                .assertValueCount(90)
+    }
+}
