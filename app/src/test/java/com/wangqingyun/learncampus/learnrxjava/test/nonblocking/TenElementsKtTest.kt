c49864fd371df4d0dd221d5cad128c6d22f21d20
wangqingyun <wangqingyun1987@gmail.com>
2018-02-21 10:49:01 +0800

    added a test

--- app/src/test/java/com/wangqingyun/learncampus/learnrxjava/test/nonblocking/TenElementsKtTest.kt
+++ app/src/test/java/com/wangqingyun/learncampus/learnrxjava/test/nonblocking/TenElementsKtTest.kt
@@ -3,6 +3,7 @@ package com.wangqingyun.learncampus.learnrxjava.test.nonblocking
 import io.reactivex.observers.TestObserver
 import org.junit.Assert.*
 import org.junit.Test
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by wangqingyun on 21/02/2018.
@@ -29,5 +30,19 @@ class TenElementsKtTest {
         observer.assertValueCount(10)
 
         observer.assertValues(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    }
+
+    @Test
+    fun verifyOneSecond() {
+        val source = getTenElements()
+        val observer = TestObserver<Long>()
+
+        source.subscribe(observer)
+
+        observer.assertSubscribed().awaitTerminalEvent(1200, TimeUnit.MILLISECONDS)
+
+        observer.assertComplete()
+                .assertNoErrors()
+                .assertValueCount(10)
     }
 }
