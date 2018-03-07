6a97a0d44a307d54b6518c58db32d7c49fa77ac8
wangqingyun <wangqingyun1987@gmail.com>
2018-03-07 11:13:06 +0800

    added PagerAdapter

---
+++ viewpager/src/main/java/com/wangqingyun/viewpager/PagerAdapter.java
@@ -0,0 +1,15 @@
+package com.wangqingyun.viewpager;
+
+/**
+ * * <p>When you implement a PagerAdapter, you must override the following methods
+ * at minimum:</p>
+ * <ul>
+ * <li>{@link #instantiateItem(ViewGroup, int)}</li>
+ * <li>{@link #destroyItem(ViewGroup, int, Object)}</li>
+ * <li>{@link #getCount()}</li>
+ * <li>{@link #isViewFromObject(View, Object)}</li>
+ * </ul>
+ * */
+
+public class PagerAdapter {
+}
