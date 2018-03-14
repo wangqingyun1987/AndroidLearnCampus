9b14ea6f9b2e7e8663a87927c1031b3a620c847c
qingyun.wang <wangqingyun123@yahoo.com>
2018-03-14 16:52:16 +0800

    a b it of change

--- mockito-analysis/src/main/java/pax/wqy/mockito/invocation/Invocation.java
+++ mockito-analysis/src/main/java/pax/wqy/mockito/invocation/Invocation.java
@@ -4,5 +4,5 @@ package pax.wqy.mockito.invocation;
  * Created by qingyun.wang on 14/03/2018.
  */
 
-public interface Invocation extends DescribedInvocation {
+public interface Invocation extends InvocationOnMock, DescribedInvocation {
 }
