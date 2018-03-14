04109ceb7e7ed25eafc50e42c1565a7f3c554826
qingyun.wang <wangqingyun123@yahoo.com>
2018-03-14 17:18:25 +0800

    added TestDocuments

---
+++ app/src/test/java/com/wangqingyun/learncampus/TestDocuments.kt
@@ -0,0 +1,6 @@
+package com.wangqingyun.learncampus
+
+/**
+ * for mockito:           https://github.com/mockito/mockito/wiki
+ * for mockito-kotlin:    https://github.com/nhaarman/mockito-kotlin/wiki/Mocking-and-verifying
+ */
