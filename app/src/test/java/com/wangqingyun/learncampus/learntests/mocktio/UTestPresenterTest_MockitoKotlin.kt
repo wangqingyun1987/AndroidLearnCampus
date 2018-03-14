6a364ba25af04bacd3c1f54785ce7c1d4a5ec8e9
qingyun.wang <wangqingyun123@yahoo.com>
2018-03-14 20:46:11 +0800

    demonstrate argThat and check

--- app/src/test/java/com/wangqingyun/learncampus/learntests/mocktio/UTestPresenterTest_MockitoKotlin.kt
+++ app/src/test/java/com/wangqingyun/learncampus/learntests/mocktio/UTestPresenterTest_MockitoKotlin.kt
@@ -1,9 +1,11 @@
 package com.wangqingyun.learncampus.learntests.mocktio
 
 import com.nhaarman.mockito_kotlin.*
+import org.hamcrest.core.Is.`is`
 import org.junit.Test
+import org.junit.Assert.assertThat
 
-@Suppress("UNUSED_ANONYMOUS_PARAMETER", "ClassName")
+@Suppress("UNUSED_ANONYMOUS_PARAMETER", "ClassName", "FunctionName")
 class UTestPresenterTest_MockitoKotlin {
     @Test
     fun verifyUtestPresenter() {
@@ -12,5 +14,25 @@ class UTestPresenterTest_MockitoKotlin {
         val presenter = UTestPresenter(view)
         presenter.onMeTouched()
         verify(view, times(1)).showTouchText(any())
+    }
+
+    @Test
+    fun verifyUtestPresenter_argThat() {
+        val view = mock<UTestContract.View>()
+        whenever(view.isUIReady()).thenAnswer { invocation -> true }
+        val presenter = UTestPresenter(view)
+        presenter.onMeTouched()
+        verify(view).showTouchText(argThat { length == 10 })
+    }
+
+    @Test
+    fun verifyUtestPresenter_check() {
+        val view = mock<UTestContract.View>()
+        whenever(view.isUIReady()).thenAnswer { invocation -> true }
+        val presenter = UTestPresenter(view)
+        presenter.onMeTouched()
+        verify(view).showTouchText(check {
+            assertThat(it.length, `is`(10))
+        })
     }
 }
