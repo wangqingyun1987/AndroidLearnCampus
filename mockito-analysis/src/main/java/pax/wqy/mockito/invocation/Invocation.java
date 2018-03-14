27f4aabd02efb1ce29c186a3f1568e268a1a9136
qingyun.wang <wangqingyun123@yahoo.com>
2018-03-14 16:59:00 +0800

    added some methods into Invocation

--- mockito-analysis/src/main/java/pax/wqy/mockito/invocation/Invocation.java
+++ mockito-analysis/src/main/java/pax/wqy/mockito/invocation/Invocation.java
@@ -5,4 +5,30 @@ package pax.wqy.mockito.invocation;
  */
 
 public interface Invocation extends InvocationOnMock, DescribedInvocation {
+    /**
+     * whether the invocation has already been verified
+     * */
+    boolean isVerified();
+
+    /**
+     * the sequence number of the invocation, used to determine the invocation order.
+     * */
+    int getSequenceNumber();
+
+    /**
+     * location in code for this invocation
+     * */
+    Location getLocation();
+
+    /**
+     * get unprocessed arguments whereas @getArguments returned processed arguments (varags expanded etc.)
+     * */
+    Object[] getRawArguments();
+
+    Class<?> getRawReturnType();
+
+    /**
+     * mark this invocation as verified
+     * */
+    void marjVerified();
 }
